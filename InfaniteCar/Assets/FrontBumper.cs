3110c102609349b742f845c399d61ab80c127961
jacob drummond <drummond.jacob13@gmail.com>
2019-09-25 10:01:17 -0400

    bumpers shit

---
+++ InfaniteCar/Assets/FrontBumper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontBumper : MonoBehaviour
+{
+    TileMover tilemover;
+    
+
+    void Start()
+    {
+        tilemover = TileMover.instance;
+        
+
+    }
+
+
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Building")
+        {
+            //push the player to the left
+            Debug.Log("fuck i hit a building F");
+            tilemover.PlayerHitCar();
+            player
+
+        }
+    }
+}
